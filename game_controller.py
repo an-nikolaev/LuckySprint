ce2735196a88ff33e112c2f0af5f43cba8653b92
iBljad <drakonmail@gmail.com>
2018-10-05 15:16:01 +0300

    tg_controller first version

--- game_controller.py
+++ game_controller.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+
+class GameController:
+    def __init__(self):
+        self._test = 0
+
+    def get_questions(self):
+        questions = \
+            {
+                0:
+                    {
+                        'question': 'Выбери расу, выбирай мудро',
+                        'answers': [('Go-Go-ерша', 0), ('Мудро', 1), ('Олег', 2)]
+                    }
+            }
+        return questions
+
+    def get_results(self, answers):
+        answers[999] = 999
+        return str(answers)
--- telegram_controller.py
+++ telegram_controller.py
@@ -4,54 +4,90 @@
 import sys
 import time
 import telepot
+import telepot.helper
+from game_controller import GameController
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-from telepot.delegate import pave_event_space, per_chat_id, create_open
+from telepot.delegate import (
+    per_chat_id, per_callback_query_origin, create_open, pave_event_space)
+
+"""
+$ python3.5 quiz.py <token>
+
+Send a chat message to the bot. It will give you a math quiz. Stay silent for
+10 seconds to end the quiz.
+
+It handles callback query by their origins. All callback query originated from
+the same chat message will be handled by the same `CallbackQueryOriginHandler`.
+"""
 
 
-# def on_chat_message(msg):
-#     content_type, chat_type, chat_id = telepot.glance(msg)
-#
-#     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-#         [InlineKeyboardButton(text='Press me', callback_data='press')],
-#     ])
-#
-#     bot.sendMessage(chat_id, 'Use inline keyboard', reply_markup=keyboard)
-
-
-class GameController(telepot.helper.ChatHandler):
+class SprintStarter(telepot.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
-        super(GameController, self).__init__(*args, **kwargs)
-        self._count = 0
+        super(SprintStarter, self).__init__(*args, **kwargs)
 
     def on_chat_message(self, msg):
-        # self._count += 1
-        # self.sender.sendMessage(self._count)
-
         content_type, chat_type, chat_id = telepot.glance(msg)
+        self.sender.sendMessage(
+            'Нажмите, чтобы начать спринт',
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[[
+                    InlineKeyboardButton(text='Начать спринт', callback_data='start'),
+                ]]
+            )
+        )
+        self.close()  # let Quizzer take over
 
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text='Press me', callback_data='press')],
-        ])
 
-        bot.sendMessage(chat_id, 'Use inline keyboard', reply_markup=keyboard)
+class Quizzer(telepot.helper.CallbackQueryOriginHandler):
+    def __init__(self, *args, **kwargs):
+        super(Quizzer, self).__init__(*args, **kwargs)
+        self.gc = GameController()
+
+        self._questions = self.gc.get_questions()
+        self._current_question_num = 0
+        self._answers = {}
+
+    def _show_next_question(self):
+
+        question = self._questions[self._current_question_num]['question']
+        choices = self._questions[self._current_question_num]['answers']
+
+        self.editor.editMessageText(question,
+                                    reply_markup=InlineKeyboardMarkup(
+                                        inline_keyboard=[
+                                            list(map(lambda c: InlineKeyboardButton(text=str(c[0]), callback_data=c[1]),
+                                                     choices))
+                                        ]
+                                    )
+                                    )
+        # return answer
 
     def on_callback_query(self, msg):
         query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-        print('Callback Query:', query_id, from_id, query_data)
 
-        bot.answerCallbackQuery(query_id, text='Got it')
+        if query_data != 'start':
+            self._answers[self._current_question_num] = query_data
+
+        if self._current_question_num < len(self._questions):
+            self._show_next_question()
+            self._current_question_num += 1
+        else:
+            self.editor.editMessageText(
+                self.gc.get_results(self._answers),
+                reply_markup=None)
+            self.close()
 
 
-TOKEN = ''
-telepot.api.set_proxy('https://198.58.10.139:51026')
+TOKEN = sys.argv[1]
 
-# bot = telepot.Bot('639608498:AAEcRyoHlVRNbx3bitaJ-XBP8vfCYahT_CA')
-
+telepot.api.set_proxy('https://190.152.14.122:32356')
 
 bot = telepot.DelegatorBot(TOKEN, [
     pave_event_space()(
-        per_chat_id(), create_open, GameController, timeout=10),
+        per_chat_id(), create_open, SprintStarter, timeout=3),
+    pave_event_space()(
+        per_callback_query_origin(), create_open, Quizzer, timeout=54000),
 ])
 
 MessageLoop(bot).run_as_thread()
